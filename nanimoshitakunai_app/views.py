1d31ab8734d9532c419ab7079363c9f2e66482a6
hitsuji-ga-u <radplankton@yahoo.co.jp>
2022-12-27 17:01:34 +0900

    comment ANIMAL

--- nanimoshitakunai_app/views.py
+++ nanimoshitakunai_app/views.py
@@ -39,84 +39,84 @@ def animals_index(request):
     #     ANIMALS[i]['skill'] = cat['category']
 
 
-    ANIMALS_ = [
+    # # ANIMALS_ = [
 
-        {'name': '羊1',
-         'id': 'hitsuiji1',
-         'path': 'nanimoshitakunai/imgs/sheep1@2x.png',
-         'info': '目つきがわるい',
-         'skill': 'なし',
-         'comment': 'こっちみんな！'
-         },
-        {
-            'name': '羊2',
-            'id': 'hitsuiji2',
-            'path': 'nanimoshitakunai/imgs/sheep2@2x.png',
-            'info': '耳がうさぎみたい',
-            'skill': 'なし',
-            'comment': ''
-        },
-        {
-            'name': '羊3',
-            'id': 'hitsuiji3',
-            'path': 'nanimoshitakunai/imgs/sheep3@2x.png',
-            'info': 'もこもこ',
-            'skill': 'なし',
-            'comment': ''
-        },
-        {
-            'name': '羊4',
-            'id': 'hitsuiji4',
-            'path': 'nanimoshitakunai/imgs/sheep4@2x.png',
-            'info': 'タイヤが大好き',
-            'skill': 'なし',
-            'comment': ''
-        },
-        {
-            'name': '羊5',
-            'id': 'hitsuiji5',
-            'path': 'nanimoshitakunai/imgs/sheep5@2x.png',
-            'info': 'なんだかロボットみたい',
-            'skill': 'なし',
-            'comment': ''
-        },
-        {
-            'name': '羊6',
-            'id': 'hitsuiji6',
-            'path': 'nanimoshitakunai/imgs/sheep6@2x.png',
-            'info': 'ツノがとぐろ巻いちゃってる',
-            'skill': 'なし',
-            'comment': ''
-        },
-        {
-            'name': '羊7',
-            'id': 'hitsuiji7',
-            'path': 'nanimoshitakunai/imgs/sheep7@2x.png',
-            'info': '優しい',
-            'skill': 'モップのふり',
-            'comment': ''
-        },
-        {
-            'name': '羊9',
-            'id': 'hitsuiji9',
-            'path': '羊9のパス',
-            'skill': 'なし',
-            'info': 'なし',
-            'comment': 'こっち見るな'
-        },
-        {
-            'name': '牧場いぬ',
-            'id': 'bokujouinu',
-            'path': '牧場犬のパス',
-            'skill': '走ること',
-            'info': 'なし',
-            'comment': 'わん！！わん！！！！'
-        },
-    ]
+    # #     {'name': '羊1',
+    # #      'id': 'hitsuiji1',
+    # #      'path': 'nanimoshitakunai/imgs/sheep1@2x.png',
+    # #      'info': '目つきがわるい',
+    # #      'skill': 'なし',
+    # #      'comment': 'こっちみんな！'
+    # #      },
+    # #     {
+    # #         'name': '羊2',
+    # #         'id': 'hitsuiji2',
+    # #         'path': 'nanimoshitakunai/imgs/sheep2@2x.png',
+    # #         'info': '耳がうさぎみたい',
+    # #         'skill': 'なし',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊3',
+    # #         'id': 'hitsuiji3',
+    # #         'path': 'nanimoshitakunai/imgs/sheep3@2x.png',
+    # #         'info': 'もこもこ',
+    # #         'skill': 'なし',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊4',
+    # #         'id': 'hitsuiji4',
+    # #         'path': 'nanimoshitakunai/imgs/sheep4@2x.png',
+    # #         'info': 'タイヤが大好き',
+    # #         'skill': 'なし',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊5',
+    # #         'id': 'hitsuiji5',
+    # #         'path': 'nanimoshitakunai/imgs/sheep5@2x.png',
+    # #         'info': 'なんだかロボットみたい',
+    # #         'skill': 'なし',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊6',
+    # #         'id': 'hitsuiji6',
+    # #         'path': 'nanimoshitakunai/imgs/sheep6@2x.png',
+    # #         'info': 'ツノがとぐろ巻いちゃってる',
+    # #         'skill': 'なし',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊7',
+    # #         'id': 'hitsuiji7',
+    # #         'path': 'nanimoshitakunai/imgs/sheep7@2x.png',
+    # #         'info': '優しい',
+    # #         'skill': 'モップのふり',
+    # #         'comment': ''
+    # #     },
+    # #     {
+    # #         'name': '羊9',
+    # #         'id': 'hitsuiji9',
+    # #         'path': '羊9のパス',
+    # #         'skill': 'なし',
+    # #         'info': 'なし',
+    # #         'comment': 'こっち見るな'
+    # #     },
+    # #     {
+    # #         'name': '牧場いぬ',
+    # #         'id': 'bokujouinu',
+    # #         'path': '牧場犬のパス',
+    # #         'skill': '走ること',
+    # #         'info': 'なし',
+    # #         'comment': 'わん！！わん！！！！'
+    # #     },
+    # # ]
 
-    context = {
-        'animals': ANIMALS_
-    }
+    # context = {
+    #     'animals': ANIMALS_
+    # }
 
 
     return render(request, 'nanimoshitakunai/animals-index.html')
